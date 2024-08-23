c2c182140b4b77a2df5ffbfb086775c8cab8572e
rajputvishal1124 <vishalsingh2962003@gmail.com>
2024-08-23 16:17:45 +0530

    completed

--- app.js
+++ app.js
@@ -2,33 +2,28 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http");
-
 const socketio = require("socket.io");
 
 const server = http.createServer(app);
-
 const io = socketio(server);
 
-
-app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-io.on("connection", function(socket){
-  socket.on("send-location", function(data){
-    io.emit("receive-location", {   id:socket.id, ...data});
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
     console.log(data);
-    console.log("newuser connected");
+    console.log("new user connected");
   });
-  socket.on("disconnect", function(){
-    console.log("User disconnected");
+  socket.on("disconnect", function () {
+    io.emit("user-disconnect", socket.id);
   });
 });
 
-
-
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   res.render("index");
 });
 
